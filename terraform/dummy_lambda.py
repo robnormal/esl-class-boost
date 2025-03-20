9dc1c9fac5557c62c55d182ff476b2ebb11f9e28
Rob Rosenbaum <rob@robrosenbaum.com>
2025-03-20 18:42:53 -0400

    Fix lambdas terraform

--- terraform/dummy_lambda.py
+++
@@ -1,2 +0,0 @@
-def lambda_handler(event, context):
-    return f"Version 1: {type(event)}, {type(context)}"
--- terraform/lambdas-sqs.tf
+++ terraform/lambdas-sqs.tf
@@ -8,6 +8,7 @@ resource "aws_lambda_function" "flask_lambda" {
   runtime       = "python3.13"
   handler       = "app.lambda_handler" # Update based on your Flask app structure
   filename      = "flask_lambda.zip"   # You'll need to create this deployment package
+  source_code_hash = filebase64sha256("flask_lambda.zip")
   tags          = local.common_tags
 
   # You can also use S3 for larger deployment packages
@@ -30,9 +31,10 @@ resource "aws_lambda_function" "flask_lambda" {
 resource "aws_lambda_function" "paragraphs_lambda" {
   function_name = "history-learning-paragraphs"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
+  handler       = "lambda_function.handler"
   runtime       = "python3.13"
   filename      = "paragraphs_lambda.zip" # Ensure this file is packaged correctly
+  source_code_hash = filebase64sha256("paragraphs_lambda.zip")
   tags          = local.common_tags
 }
 
@@ -47,18 +49,20 @@ resource "aws_lambda_permission" "allow_s3" {
 resource "aws_lambda_function" "vocabulary_lambda" {
   function_name = "history-learning-vocabulary"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
+  handler       = "lambda_function.handler"
   runtime       = "python3.13"
   filename      = "vocabulary_lambda.zip"
+  source_code_hash = filebase64sha256("vocabulary_lambda.zip")
   tags          = local.common_tags
 }
 
 resource "aws_lambda_function" "summaries_lambda" {
   function_name = "history-learning-summaries"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
+  handler       = "lambda_function.handler"
   runtime       = "python3.13"
   filename      = "summaries_lambda.zip"
+  source_code_hash = filebase64sha256("summaries_lambda.zip")
   tags          = local.common_tags
 }
 
