61c2f60f12d48b5de08021d2636d63c9e96e40fe
Rob Rosenbaum <rob@robrosenbaum.com>
2025-03-23 19:18:35 -0400

    Add packaging and deployment scripts

---
+++ deploy-react.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# build-and-deploy.sh
+
+# Stop on error
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"/terraform
+CLOUDFRONT_DIST_ID=$(terraform output -raw cloudfront_distribution_id)
+
+cd ../frontend
+
+# Step 1: Build the React app
+npm run build
+
+# Step 2: Upload to S3 (sync deletes files no longer in the build folder)
+aws s3 sync build/ s3://rhr79-history-learning-website --delete
+
+# Step 3: Invalidate CloudFront cache
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths "/*"
