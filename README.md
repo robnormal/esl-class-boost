e87f02fc6b553f969b6b6aaa29e6c0b03fa6dcf3
Rob Rosenbaum <rob@robrosenbaum.com>
2025-06-12 22:21:14 -0400

    Add script for dev start and a main README

---
+++ README.md
@@ -0,0 +1,17 @@
+To launch everything locally:
+
+```shell
+bash local_start.sh
+backend/start_service.sh api
+backend/start_service.sh paragraphs
+backend/start_service.sh vocabulary
+backend/start_service.sh summaries
+```
+
+To put into production:
+
+```shell
+
+bash deploy-react.sh
+
+```
---
+++ local_start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"/infra/dev
+
+docker start backend-localstack-1
+
+# Delete terraform files except main.tf
+find . -maxdepth 1 -type f -name '*.tf' ! -name 'main.tf' -delete
+
+# Copy terraform files from parent
+cp ../db.tf ../s3.tf ../sqs.tf .
+
+terraform init
+terraform apply --auto-approve
