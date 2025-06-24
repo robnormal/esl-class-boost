b5559103d1331c27cddabb5bc7f1cf413831fe30
Rob Rosenbaum <rob@robrosenbaum.com>
2025-06-24 14:45:37 -0400

    Use explicit version numbers with ECR images
    This allows for predictable and reproducible deploys, as compared to
    using "latest"

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -10,9 +10,22 @@ AWS_REGION="us-east-2"  # Change this to your region
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 ECR_REGISTRY="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 BUILD_CONTEXT=$(pwd) # Docker build context has to include both the service code and `common`
+SERVICES_DIR=services
 
 # Array of services
-SERVICES=("api" "paragraphs" "summaries" "vocabulary")
+ALL_SERVICES=("api" "paragraphs" "summaries" "vocabulary")
+
+if [[ $# -ge 1 && -n "$1" ]]; then
+    if [[ ! -d "$SERVICES_DIR/$1" ]]; then
+        echo "❌ Error: Service directory '$SERVICES_DIR/$1' does not exist."
+        exit 1
+    else
+        SERVICES=("$1")
+        VERSION="${2:-latest}"
+    fi
+else
+    SERVICES=("${ALL_SERVICES[@]}")
+fi
 
 # Function to check if a command exists
 command_exists() {
@@ -43,7 +56,7 @@ fi
 echo "🔑 Logging into ECR..."
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$ECR_REGISTRY"
 
-cd services
+cd "$SERVICES_DIR"
 
 # Process each service
 for service in "${SERVICES[@]}"; do
@@ -52,15 +65,15 @@ for service in "${SERVICES[@]}"; do
 
     # Build Docker image
     echo "🏗️  Building Docker image for $service..."
-    docker build -f "Dockerfile.$service" -t "$REGISTRY_TAG" "$BUILD_CONTEXT"
+    docker build -f "Dockerfile.$service" -t "$REGISTRY_TAG:$VERSION" "$BUILD_CONTEXT"
 
     # Tag image for ECR
     echo "🏷️  Tagging image for ECR..."
-    docker tag "$REGISTRY_TAG:latest" "$ECR_REGISTRY/$REGISTRY_TAG:latest"
+    docker tag "$REGISTRY_TAG:$VERSION" "$ECR_REGISTRY/$REGISTRY_TAG:$VERSION"
 
     # Push to ECR
     echo "⬆️  Pushing image to ECR..."
-    docker push "$ECR_REGISTRY/$REGISTRY_TAG:latest"
+    docker push "$ECR_REGISTRY/$REGISTRY_TAG:$VERSION"
 
     echo "✅ Completed processing $service service"
 done
